491640d61ce6eaa0e2b207cce8d06e8058ed7ff9
Prince Rana <ranaprince45may@gmail.com>
2026-04-01 19:15:01 +0530

    Implemented Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,25 @@ import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Loading from "./Pages/Loading";
 import VerifyOTP from "./Pages/VerifyOtp";
+import ProtectedRoute from "./Private/ProtectedRoute";
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Loading />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/VerifyOtp" element={<VerifyOTP />} />
+
+        {/*  Protected Routes Group */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/profile" element={<Profile />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/table/:id" element={<GameTable />} /> */}
+        </Route>
       </Routes>
     </>
   );
---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchProfile = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/v1/profile");
+      setUser(res.data);
+    } catch (err) {
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(()=>{
+    fetchProfile()
+  },[])
+
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export const useAuth = () => useContext(AuthContext);
+export default AuthProvider;
